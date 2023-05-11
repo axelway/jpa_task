954084c2f0a5fad4404e9e1ad183a9d9e9409801
Александр <progzmaker@gmail.com>
2023-05-11 17:55:11 +0300

    Initial commit

---
+++ src/test/java/com/pluralsight/conference/ConferenceApplicationTests.java
@@ -0,0 +1,13 @@
+apackage com.pluralsight.conference;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConferenceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
